1c77a43e6d623a87ab02dbf6287d6e536257ca74
han <walcoorperation@gmail.com>
2023-06-07 02:11:06 +0800

    Updates

---
+++ precache-manifest.5597e1effe7d713124b1e0de719fe8cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d64bb7f9b25c05b00cf3afbb011b79",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3ab2d8ecea278473cb4",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "6e8b5ce88db2423ab761",
+    "url": "./static/js/2.956d16eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.956d16eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ab2d8ecea278473cb4",
+    "url": "./static/js/main.8f2a8880.chunk.js"
+  },
+  {
+    "revision": "83450937777ffd93cc09",
+    "url": "./static/js/runtime-main.405c1bd9.js"
+  }
+]);
